eaca53a3b0b01fbab14755319bcd879a51a9793d
Unknown <harryjrobertson@gmail.com>
2017-10-24 19:46:08 +0100

    Re structures classes

--- src/IRCClient.java
+++ src/IRCClient.java
@@ -1,7 +1,26 @@
-public class IRCClient {
+import java.net.*;
+import java.io.*;
 
+public class IRCClient {
+	private ServerSocket serverSocket;
+	
 	public static void main(String[] args) {
 		System.out.println("Running Client");
 	}
 
+	private class clientConnection implements Runnable {
+		
+		private Socket clientSocket;
+		
+		public clientConnection(Socket socket) {
+			this.clientSocket = socket;
+		}
+
+		@Override
+		public void run() {
+			
+		}
+		
+	}
+	
 }
--- src/IRCServer.java
+++ src/IRCServer.java
@@ -1,7 +1,52 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class IRCServer {
 
-	public static void main(String[] args) {
-		System.out.println("Running Server");
+	private ServerSocket serverSocket;
+	private String serverName;
+	
+	public IRCServer() {
+		this.serverName = "Unnamed Server";
+	}
+	
+	public IRCServer(String serverName) {
+		this.serverName = serverName;
 	}
 
+	public void makeSocket(int port) {
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void start() {
+		
+	}
+	
+	private class clientConnection implements Runnable {
+		
+		private Socket clientSocket;
+		
+		public clientConnection(Socket socket) {
+			this.clientSocket = socket;
+		}
+
+		@Override
+		public void run() {
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Running Server");
+		if (args[0] != null) {
+			IRCServer myServer = new IRCServer();
+			myServer.makeSocket(Integer.parseInt(args[0]));
+			myServer.start();
+		} else { System.out.println("Usage: IRCServer <Port>"); }
+	}
 }
