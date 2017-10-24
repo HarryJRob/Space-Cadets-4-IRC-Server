119a3910c94928a2036b2939f048ddaadb37dd60
Unknown <harryjrobertson@gmail.com>
2017-10-24 21:15:08 +0100

    Work on setting up connections

--- src/IRCClient.java
+++ src/IRCClient.java
@@ -2,25 +2,36 @@ import java.net.*;
 import java.io.*;
 
 public class IRCClient {
-	private ServerSocket serverSocket;
+	
+	private Socket clientSocket;
+	private PrintWriter clientOutput;
+	private BufferedReader clientInput;
 	
 	public static void main(String[] args) {
 		System.out.println("Running Client");
+		IRCClient myClient = new IRCClient();
+		myClient.run();
 	}
 
-	private class clientConnection implements Runnable {
-		
-		private Socket clientSocket;
-		
-		public clientConnection(Socket socket) {
-			this.clientSocket = socket;
-		}
-
-		@Override
-		public void run() {
+	public void run() {
+		setSocket("localhost", 2004);
+		try {
+			clientOutput = new PrintWriter(clientSocket.getOutputStream(), true);
+			clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			
+			//clientOutput.println("Hello, World");
+		} catch (IOException e) {
+			System.out.println("Cannot find server");
+			System.out.println(e.toString());
 		}
-		
+	}
+	
+	private void setSocket(String ip, int port) {
+		try {
+			clientSocket = new Socket(ip,port);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
 	}
 	
 }
--- src/IRCServer.java
+++ src/IRCServer.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -24,26 +27,51 @@ public class IRCServer {
 	}
 	
 	public void start() {
-		
+		int count = 0;
+		while (count != 5) {
+            try {
+				new clientConnection(serverSocket.accept()).run();
+				count++;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Server ending");
 	}
 	
 	private class clientConnection implements Runnable {
 		
 		private Socket clientSocket;
+		private PrintWriter clientOutput;
+		private BufferedReader clientInput;
+		private String nickname;
 		
 		public clientConnection(Socket socket) {
 			this.clientSocket = socket;
+
 		}
 
 		@Override
 		public void run() {
-			
+			try {
+				this.clientOutput = new PrintWriter(clientSocket.getOutputStream(), true);
+				this.clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				
+				clientOutput.println(serverName);
+				System.out.println("Connection from: " + clientSocket.getInetAddress() + " : " + clientSocket.getPort());
+				
+				
+				
+			} catch (IOException e) {
+				System.out.println(e.toString());
+			}
 		}
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Running Server");
-		if (args[0] != null) {
+		if (args.length == 1) {
 			IRCServer myServer = new IRCServer();
 			myServer.makeSocket(Integer.parseInt(args[0]));
 			myServer.start();
